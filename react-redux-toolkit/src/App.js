22edb2f80f6c832faf3cb77c76948ebba689841b
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-01 14:51:18 +0530

    updated

--- react-redux-toolkit/src/App.js
+++ react-redux-toolkit/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+
+  const [count,setCount] = useState(0);
+
+  function incr(){
+    setCount(count+1);
+  }
+  function decr(){
+    setCount(count-1);
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incr}>+</button>
+      <br />
+      <button onClick={decr}>-</button>
+    </div>
   )
 }
 
